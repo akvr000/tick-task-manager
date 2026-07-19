99c72cb505e0bb5573e0121514fc085c85b8de5b
akvr000 <akvr46921@gmail.com>
2026-07-19 13:31:23 +0530

    add vercel web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Analytics } from "@vercel/analytics/react";
 import Header from './components/Header';
 import TaskInput from './components/TaskInput';
 import TaskList from './components/TaskList';
